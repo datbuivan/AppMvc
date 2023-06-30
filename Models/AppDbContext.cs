7bc801d484ff39cd338a75e2e73bd62938912609
datbuivan <datbuivan229@gmail.com>
2023-06-30 22:16:03 +0700

    addContact

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using AppMvc.Models.Contacts;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppMvc.Models
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+            base.OnModelCreating(builder);
+            // Bỏ tiền tố AspNet của các bảng: mặc định
+            foreach (var entityType in builder.Model.GetEntityTypes())
+            {
+                var tableName = entityType.GetTableName();
+                if (tableName.StartsWith("AspNet"))
+                {
+                    entityType.SetTableName(tableName.Substring(6));
+                }
+            }
+        }
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
